92beb536f5c2248a9e52702375fd49da5aa38c43
divinity360 <thedivinitysoft@gmail.com>
2022-08-03 20:21:00 +0100

    initialize project architecture

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ubenwa/providers/pet_provider.dart';
+import 'package:ubenwa/utils/routes.dart';
+import 'package:ubenwa/utils/theme.dart';
+
+/// Global navigator state
+GlobalKey<NavigatorState> navigationKeyState = GlobalKey<NavigatorState>();
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider<PetProvider>(
+      create: (context) => PetProvider(),
+    ),
+  ], child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Figozo Test',
+        theme: lightTheme(context),
+        navigatorKey: navigationKeyState,
+        onGenerateRoute: AppRoutes.generateRoute);
+  }
+}
